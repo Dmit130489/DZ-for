878c27db8e2211383e44c44269fbb953191a7dc0
Dmit130489 <dmit.89@list.ru>
2024-05-24 20:22:25 +0300

    Add files via upload

---
+++ DZ цикл for.py
@@ -0,0 +1,8 @@
+car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in car:
+    print('Я езжу на автомабиле марки', i)
+for i in range(len(car)):
+    cars_count += 10
+print(cars_count)
+
